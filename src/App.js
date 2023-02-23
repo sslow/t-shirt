4233866f507bacbdcf1f7a69ae97d8c8427d5df2
sslow <showllyxiang@163.com>
2023-02-23 14:06:16 +0800

    test upload

--- src/App.js
+++ src/App.js
@@ -5,9 +5,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          T-shirt
         </p>
         <a
           className="App-link"
@@ -15,7 +14,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn More
         </a>
       </header>
     </div>
